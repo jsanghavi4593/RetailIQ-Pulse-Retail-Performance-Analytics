770ce0be87cfc667cd95a4e6791b101a3100571a
Jainil Sanghavi <82511070+jsanghavi4593@users.noreply.github.com>
2026-01-19 11:24:21 +0530

    Add files via upload

---
+++ Databases/regions_RetailIQ.sql
@@ -0,0 +1,11 @@
+INSERT INTO Regions (RegionID, RegionName) VALUES
+(1,'North'),
+(2,'South'),
+(3,'East'),
+(4,'West'),
+(5,'Central'),
+(6,'North-East'),
+(7,'South-East'),
+(8,'North-West'),
+(9,'South-West'),
+(10,'International');
